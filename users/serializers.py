0064ffa644a42234cd72f503fef568e17df3761e
Lucas Batista <batista.lucas3098@gmail.com>
2022-07-22 11:15:50 -0300

    feature/users: basic CRUD creation

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,3 @@
-from dataclasses import fields
 from rest_framework import serializers
 from user_numbers.serializers import UserNumbersSerializer
 
@@ -8,9 +7,8 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        exclude = ['last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff', 'is_active', 'groups', 'user_permissions', 'date_joined']
         extra_kwargs = {"password": {"write_only": True}}
-
         read_only_fields=["id", "created_at"]
     
     user_numbers = UserNumbersSerializer(many=True, read_only=True)
