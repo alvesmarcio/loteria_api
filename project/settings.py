a38b180298822172a2e25b8a77fe989dde642e1b
Márcio Alves <alvesmarcio@hotmail.com>
2022-07-22 10:17:35 -0300

    fix: fix dotenv imports

--- project/settings.py
+++ project/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from os import getenv
+import os
 from pathlib import Path
-from dotenv import load_dotenv
+import dotenv
 
-load_dotenv()
+dotenv.load_dotenv(dotenv.find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = getenv('DEBUG', True)
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
