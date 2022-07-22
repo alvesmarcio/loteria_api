53f56156babc8d830be059780446038f22536f56
Márcio Alves <alvesmarcio@hotmail.com>
2022-07-22 10:03:53 -0300

    feature: ids changed to uuidfield

--- user_numbers/models.py
+++ user_numbers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from uuid import uuid4
 
 class UserNumbersModel(models.Model):
-    id = models.AutoField(primary_key=True, default=uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)    
     numbers = models.CharField(max_length=100)
     favorite = models.BooleanField(default=False)
     
