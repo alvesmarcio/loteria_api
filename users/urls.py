e032a17151047caddbeb3415e12fd8a1bdd5c499
Márcio Alves <alvesmarcio@hotmail.com>
2022-07-25 11:27:50 -0300

    feat: Procfile

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from users.views import LoginView, UserIdView, UserNumberView, UserView
 
 urlpatterns = [
-    path('users/', UserView.as_view()),
-    path('users/<str:id>/', UserIdView.as_view()),  
-    path('users/<str:id>/number/', UserNumberView.as_view()),    
-    path('login/', LoginView.as_view()),
+    path("users/", UserView.as_view()),
+    path("users/<str:id>/", UserIdView.as_view()),
+    path("users/<str:id>/number/", UserNumberView.as_view()),
+    path("login/", LoginView.as_view()),
 ]
