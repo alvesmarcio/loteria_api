84d9cda8babbe50cb7e677f85bf8af4ee4e811c8
Diego <diegohpsousa@gmail.com>
2022-07-27 15:51:57 -0300

    feat/user_numbers_tests

--- user_numbers/tests.py
+++ user_numbers/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+class YourTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_something_that_will_pass(self):
+        self.assertFalse(False)
+
+    def test_something_that_will_fail(self):
+        self.assertTrue(False)
